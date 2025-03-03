5a4f0cc2bc1c04412d0cf035bb2615b08d2407dc
woongaaaa <woonge0930@naver.com>
2025-03-04 03:09:07 +0900

    [Setting] Add color assets

--- SpotCalendar/Sources/SwiftUIView.swift
+++ SpotCalendar/Sources/SwiftUIView.swift
@@ -16,6 +16,7 @@ public struct SwiftUIView: View {
     public var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
             .font(.H6)
+            .foregroundStyle(.blue300)
     }
 }
 
