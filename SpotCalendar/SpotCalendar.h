1f28280ae05d3e9f9a8176e0d9f0253bc6a9662b
woongaaaa <woonge0930@naver.com>
2025-03-04 02:08:03 +0900

    [Setting] Make framework project

---
+++ SpotCalendar/SpotCalendar.h
@@ -0,0 +1,18 @@
+//
+//  SpotCalendar.h
+//  SpotCalendar
+//
+//  Created by 황채웅 on 3/4/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SpotCalendar.
+FOUNDATION_EXPORT double SpotCalendarVersionNumber;
+
+//! Project version string for SpotCalendar.
+FOUNDATION_EXPORT const unsigned char SpotCalendarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpotCalendar/PublicHeader.h>
+
+
